d718dd9d302004a58ebbc583261f3e729a034940
MB Phong Ram <mabuphongram171@gmail.com>
2023-10-07 20:15:23 -0400

    cleaning up our Routes

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:freecodecamp/constants/route.dart';
 import 'package:freecodecamp/firebase_options.dart';
 import 'dart:developer' as devtools show log;
 
@@ -81,7 +82,7 @@ class _RegisterViewState extends State<RegisterView> {
           TextButton(
             onPressed: () {
               Navigator.of(context).pushNamedAndRemoveUntil(
-                '/login/',
+                loginRoute,
                 (route) => false,
               );
             },
