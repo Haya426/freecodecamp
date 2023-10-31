011d9e677df90aab50fe176693af308803e2aad1
MB Phong Ram <mabuphongram171@gmail.com>
2023-10-31 17:21:05 -0400

    Abstract class- step 2

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,29 @@ import 'dart:developer' as devtools show log;
 extension Log on Object{
   void log() => devtools.log(toString());
 }
+// 'Abstract' mean no logic or empty
+// 'no abstract' means logic
 
 abstract class CanRun{
-  void run();
+  @mustCallSuper // it push to invoke super class, 
+  void run(){
+    "can run function is called".log();
+  } // normal function
 }
 
-// extends keyword can extend one abstract class
-// To extend mutiple classes, 'with' keyword must be applied
 class Cat extends CanRun {
   @override
-  void run() {
-    // TODO: implement run
-  }
+  void run() { // If empty body does not invoke super class
+    super.run();
+    'cat running'.log();
 
+  }
 }
+
+void testIt(){
+    final cat  = Cat();
+    cat.run();
+  }
 
 void main() {
   runApp(MyApp());
@@ -26,6 +35,10 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    //invoke test function
+    testIt();
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
