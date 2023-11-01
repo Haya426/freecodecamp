5746b8664edb809658ac8c5b2b98144b2c3600ed
MB Phong Ram <mabuphongram171@gmail.com>
2023-10-31 20:29:16 -0400

    Mixin - step -1

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,43 @@
 import 'package:flutter/material.dart';
 import 'dart:developer' as devtools show log;
 
-extension Log on Object{
+extension Log on Object {
   void log() => devtools.log(toString());
 }
-// mixin mean to implement functions from many abstract classes
-abstract class CanRun{
 
+// mixins are abstract class that cannot have constructor
+mixin CanRun {
+  int get speed;
+
+  // not abstract function, but logic function
   void run(){
-    'Running...'.log();
-  } 
-}
-abstract class CanWalk {
-  void walk(){
-    'Walking...'.log();
+    'Running at the speed of $speed'.log();
   }
 }
 
-class Cat with CanRun, CanWalk {} //abstract class မှာ contructor မရှိလို့ mixin လုပ်လို့ရတာဖြစ်
+class Cat with CanRun {
+  
+  //speed ကရှိကိုရှိရမယ်-ဒီလို assign လုပ်တယ်
+  @override
+  int speed=10;
 
-
-
-void testIt(){
-   
-   final cat = Cat();
-   cat
-   ..run()
-   ..walk();
-  }
-
+}
+//to test
+void testIt() {
+final cat = Cat();
+cat.run();
+cat.speed = 20;
+cat.run();
+}
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
-
-    //invoke test function
     testIt();
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
