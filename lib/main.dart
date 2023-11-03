14ad48d5ad02f0711c0d753ec4b86a037246ffe8
MB Phong Ram <mabuphongram171@gmail.com>
2023-11-02 22:36:14 -0400

    Isolate - feching data with press me button

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
-import 'package:meta/meta.dart';
+import 'dart:isolate';
 import 'package:flutter/material.dart';
 import 'dart:developer' as devtools show log;
 
@@ -8,56 +8,60 @@ extension Log on Object {
   void log() => devtools.log(toString());
 }
 
-//Mixins together with extensions are very powerful
+//isolate အကြောင်းဖတ်မယ်
+// event loop အကြောင်းဖတ်မယ်
+// main isolate က app UI အတွက်သုံးတယ် 
+//isolate တစ်ခုနဲ့တစ်ခု message passing အတွက် port ကိုသုံးတယ်
 
-extension GetOnUri on Object {
-  Future<HttpClientResponse> getUrl(String url) 
-  =>HttpClient()
-  .getUrl(Uri.parse(url))
-  .then((req) => req.close());
 
-}
-
-mixin CanMakeGetCall {
-  String get url;
-  
-  @useResult
-  Future<String> getString()=>
-  getUrl(url)
-  .then((res) => res.transform(utf8.decoder).join());
-}
-
-@immutable
-class GetPeople with CanMakeGetCall {
-
-  
-  @override
- 
-  String get url => 'http://127.0.0.1:5500/api/people.json';
-  
-}
-//to test
-void testIt() async{
-final people = await  GetPeople().getString();
-people.log();
-}
 void main() {
   runApp(MyApp());
 }
 
+@immutable
+class Person {
+  final String name;
+  final int age;
+
+ const Person({
+  required this.name, 
+  required this.age});
+  Person.fromJson(Map<String,dynamic> json):
+  name = json["name"] as String,
+  age = json["age"] as int;
+}
+
+Future<Iterable<Person>> getPersons() async {
+  final rp = ReceivePort();
+  await Isolate.spawn(_getPersons,rp.sendPort);
+  return await rp.first; // receipt port is stream 
+}
+//define function to send value via port
+void _getPersons(SendPort sp) async {
+  const url = 'http://127.0.0.1:5500/apis/people1.json';
+  final persons = await HttpClient()
+  .getUrl(Uri.parse(url))
+  .then((req) => req.close())
+  .then((reponse) => reponse.transform(utf8.decoder).join())
+  .then((jsonString) => json.decode(jsonString)as List<dynamic>)
+  .then((json) => json.map((map) => Person.fromJson(map)));
+  
+  Isolate.exit(sp,persons);
+}
 class MyApp extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    testIt();
+    
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Flutter App'),
         ),
-        body: const Center(
-          child: Text('Hello, Flutter!'),
-        ),
+        body: TextButton(onPressed: () async{
+          final persons = await getPersons();
+          persons.log();
+        }, child: const Text('Press me'))
       ),
     );
   }
